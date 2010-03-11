66e4530373215f02f24a95e896d102bca6de53ca
Henning Hraban Ramm <hraban@fiee.net>
2010-03-11 14:02:03 +0100

    new: t-LilyPond ConTeXt module

---
+++ t-lilypond/makezip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MODNAME=lilypond
+BASEDIR=~/workspace/context/t-${MODNAME}/
+SUBDIR=/context/third/${MODNAME}/
+cd ${BASEDIR}tex${SUBDIR}
+context t-${MODNAME} --mode=demo --result=demo
+texexec --luatex --module t-${MODNAME}
+context --purge
+mv *.pdf ${BASEDIR}/doc/${SUBDIR}
+cd ${BASEDIR}
+zip -u ../t-${MODNAME}-`date +"%Y-%m-%d"`.zip doc${SUBDIR}*.pdf
+zip -u ../t-${MODNAME}-`date +"%Y-%m-%d"`.zip tex${SUBDIR}t-${MODNAME}.tex
