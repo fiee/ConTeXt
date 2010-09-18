cc1da59fb6775e39852618c037c227347bc62cfc
hraban <hraban@fiee.net>
2010-09-18 23:39:57 +0200

    Started to make a new version, but didn't come far

--- t-lilypond/makezip.sh
+++ t-lilypond/makezip.sh
@@ -4,7 +4,7 @@ BASEDIR=~/workspace/context/t-${MODNAME}/
 SUBDIR=/context/third/${MODNAME}/
 cd ${BASEDIR}tex${SUBDIR}
 context t-${MODNAME} --mode=demo --result=demo
-texexec --luatex --module t-${MODNAME}
+context --ctx=s-mod.ctx t-${MODNAME}
 context --purge
 mv *.pdf ${BASEDIR}/doc/${SUBDIR}
 cd ${BASEDIR}
